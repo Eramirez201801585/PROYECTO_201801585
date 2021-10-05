8e852dce06d13347b736e5153e69976422227dc6
Eramirez201801585 <88957320+Eramirez201801585@users.noreply.github.com>
2021-10-05 00:16:05 -0600

    Delete PROYECTO_201801585 directory

--- PROYECTO_201801585/PROYECTO_201801585/frmRegistro.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Class frmRegistro
-    Private Sub btnMenu_Click(sender As Object, e As EventArgs) Handles btnMenu.Click
-        frmLogin.Show()
-        Me.Close()
-
-    End Sub
-End Class
